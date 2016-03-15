71b55a40c2aef950da29a8069750b8eef0233dd7
pablo <lobocano@msn.com>
2016-03-15 14:55:19 +0300

    login/register pages

--- server.js
+++ server.js
@@ -12,7 +12,46 @@ server.route({
     method: 'GET',
     path: '/',
     handler: function (request, reply) {
-        reply('Hello, world!');
+        reply.file('./public/index.html');
+    }
+});
+server.route({
+    method: 'GET',
+    path: '/api/users',
+    handler: function (request, reply) {
+        var u = {name: 'qq'};
+        reply(u);
+    }
+});
+server.route({
+    method: 'GET',
+    path: '/api/comments',
+    handler: function (request, reply) {
+        var cm = {time: new Date()};
+        reply(cm);
+    }
+});
+server.route({
+    method: 'GET',
+    path: '/auth/login',
+    handler: function (request, reply) {
+        reply.file('./public/login.html');
+    }
+});
+server.route({
+    method: 'GET',
+    path: '/auth/register',
+    handler: function (request, reply) {
+        reply.file('./public/register.html');
+    }
+});
+server.route({
+    method: 'POST',
+    path: '/auth/login',
+    handler: function (request, reply) {
+        console.log(request.payload);
+        var cm = {username: request.payload.username, password: request.payload.password};
+        reply(cm);
     }
 });
 server.route({
@@ -61,3 +100,4 @@ server.register({
         server.log('info', 'Server running at: ' + server.info.uri);
     });
 });
+//curl -X POST -H "Content-Type: application/json" -d '{ "username": "lksjkflksjf","password": "123456" }' -i http://comments.lobo-cano.ru/api/login
